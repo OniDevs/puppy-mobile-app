1d3df0607a07b903d30c28a5a4a2000b43b3141b
Jorge Luis Morales Centeno <jmoralescenteno@gmail.com>
2022-09-17 03:33:10 +0000

    fonts added

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
+import { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'Montserrat-Bold': require('./src/assets/fonts/Montserrat-Bold.ttf'),
+    'Montserrat-SemiBold': require('./src/assets/fonts/Montserrat-SemiBold.ttf'),
+    'Montserrat-Medium': require('./src/assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-Regular': require('./src/assets/fonts/Montserrat-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Text>Open up App.js to start working on your app!!!</Text>
+      <Text style={{ fontFamily: 'Montserrat-Bold', fontSize: 40 }}>Montserrat-Bold</Text>
+      <Text style={{ fontFamily: 'Montserrat-SemiBold' }}>Montserrat-SemiBold</Text>
+      <Text style={{ fontFamily: 'Montserrat-Medium' }}>Montserrat-Medium</Text>
+      <Text style={{ fontFamily: 'Montserrat-Regular' }}>Montserrat-Regular</Text>
       <StatusBar style="auto" />
     </View>
   );
